f732f1977becaf39b13c8beb6892fe0f7658236d
Inndy <inndy.mail@gmail.com>
2015-12-02 11:45:10 +0800

    Remove accidentially duplicated views

--- src/application/views/views/pages/post.php
+++
@@ -1,8 +0,0 @@
-<?php foreach($posts as $post): ?>
-<div class="post">
-    <p><span class="field-name">作者</span><?=html_escape($post->author)?></p>
-    <p><span class="field-name">時間</span><?=html_escape($post->time)?></p>
-    <p><span class="field-name">標題</span><?=html_escape($post->title)?></p>
-    <p><span class="field-name">內容</span><?=html_escape($post->content)?></p>
-</div>
-<?php endforeach; ?>
